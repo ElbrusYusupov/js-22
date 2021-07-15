880b791601b50b63aaa5ceb3165f06fe613491fb
ElbrusYusupov <yusupoffup@gmail.com>
2021-07-15 10:34:21 +0300

    кнке

---
+++ JS/js2-09.js
@@ -0,0 +1,10 @@
+function getExtremeElements(array) {
+  // Change code below this line
+  let FirstElements = array[0]
+  let LastElement = array[array.length -1]
+  
+  const NewMassiv = [FirstElements, LastElement]
+  
+  return NewMassiv
+  // Change code above this line
+}
